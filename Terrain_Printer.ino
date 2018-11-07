fcda1a4ca2800cc35561974b932cb04dbd051d9d
Loten T Lhatsang <42759455+low10high10@users.noreply.github.com>
2018-11-06 20:20:01 -0500

    Update Terrain_Printer.ino

--- Terrain_Printer.ino
+++ Terrain_Printer.ino
@@ -11,8 +11,8 @@ void loop() {
   int pulseX;
   int pulseY;
 
-  pulseX= pulseIn(xIn, HIGH); //read x Pulse
-  pulseY= pulseIn(yIn, HIGH); //read y Pulse
+  pulseX= pulseIn(xIn, HIGH); //reads x Pulse (Sideways Tilt)
+  pulseY= pulseIn(yIn, HIGH); //reads y Pulse (Forward & Backward Tilt)
 
   //Displays Pulse Widths
   
@@ -22,19 +22,20 @@ void loop() {
 
 
  
-
-  if((4920 < pulseX && pulseX< 5000)&& (4960 < pulseY && pulseY< 5000) ){
+  // Prints 'Flat' if in flat terrain range. 
+  if((4920 < pulseX && pulseX< 5000)&& (4960 < pulseY && pulseY< 5000) ){ 
     Serial.println("Flat");
   } 
+  
+  // Prints 'Rocky' if in Rocky terrain range. 
   else if ((4880 <pulseX ) && (5010<pulseY) || ((4870 <pulseX) && (pulseY<4850)))  {
     Serial.println("Rocky");
   }
+
+  // Prints 'Hilly' if in Hilly terrain range. 
   else if(( pulseX < 4860) && (4960<pulseY && pulseY < 5030)){
     Serial.println("Hilly");
   }
 
-  delay(1000);
-  
-  
+  delay(1000); 
 }
-
