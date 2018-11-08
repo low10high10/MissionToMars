1846dab17428b26f0e6154ba117634a07f6cca86
Oliver Rodas <44071910+Revilo2157@users.noreply.github.com>
2018-11-08 15:34:01 -0500

    Add files via upload

--- XBee.ino
+++ XBee.ino
@@ -8,30 +8,31 @@ void setup() {
   delay(500);
 
   // Setup Pins
-  pinMode(8, INPUT);
-  pinMode(9, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 
   // Turn Pins Off
-  digitalWrite(9, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(6, LOW);
   digitalWrite(4, LOW);
 }
 
 void loop() {
 
   // If button is pressed, send an "o" and turn on the yellow LED
-  if (digitalRead(8) == HIGH) {
-    char outgoing = 'o';
+  if (digitalRead(4) == HIGH) {
+    char outgoing = 'Z';
     Serial2.println(outgoing);
     Serial.println("Button Pressed");
-    digitalWrite(4, HIGH);
+    digitalWrite(6, HIGH);
     delay(200);
   }
 
   // If something is available on Serial Port 2, turn on the green LED
   if(Serial2.available()) {
     char incoming = Serial2.read();
-    digitalWrite(9, HIGH);
+    digitalWrite(7, HIGH);
     Serial.println(incoming);
   }
 }
