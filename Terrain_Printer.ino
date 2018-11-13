ed7136135b6c7312615a6e785ad554abf3c49676
Loten T Lhatsang <42759455+low10high10@users.noreply.github.com>
2018-11-12 22:24:02 -0500

    Update Terrain_Printer.ino

--- Terrain_Printer.ino
+++ Terrain_Printer.ino
@@ -13,21 +13,29 @@ void loop() {
 
   pulseX= pulseIn(xIn, HIGH); //reads x Pulse (Sideways Tilt)
   pulseY= pulseIn(yIn, HIGH); //reads y Pulse (Forward & Backward Tilt)
+
+  //Displays Pulse Widths
+  
+  Serial.print(pulseX); //Tilts Left & Right
+  Serial.print("\t");
+  Serial.println(pulseY);
+
+
  
   // Prints 'Flat' if in flat terrain range. 
-  if((4920 < pulseX && pulseX< 5000)&& (4960 < pulseY && pulseY< 5000) ){ 
+  if((4920 < pulseX && pulseX< 5100)&& (4890 < pulseY && pulseY< 5000) ){ 
     Serial.println("Flat");
   } 
   
   // Prints 'Rocky' if in Rocky terrain range. 
-  else if ((4880 <pulseX ) && (5010<pulseY) || ((4870 <pulseX) && (pulseY<4850)))  {
+  else if ((4860 <pulseX ) && (5010<pulseY) || ((4850 <pulseX) && (pulseY<4850)))  {
     Serial.println("Rocky");
   }
 
   // Prints 'Hilly' if in Hilly terrain range. 
-  else if(( pulseX < 4860) && (4960<pulseY && pulseY < 5030)){
+  else if(( pulseX < 4860) && (4870<pulseY && pulseY < 5030)){
     Serial.println("Hilly");
   }
 
-  delay(1000); 
+  delay(1000);
 }
